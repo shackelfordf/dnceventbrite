23012b0edebf313e0a01db2aae4b8ebddd42fc46
Nikki Murray <murrayn@dnc.org>
2016-03-25 14:23:51 -0400

    adding ruby version of script

---
+++ dnceventbrite.rb
@@ -0,0 +1,19 @@
+require 'eventbrite'
+require 'json_converter'
+
+Eventbrite.token = "BGRKOZMNI6NQRUNPD6BZ"
+
+events = Eventbrite::User.owned_events({ user_id: 165365079997  })
+attendees = Eventbrite::User.owned_event_attendees({ user_id: 165365079997  })
+attendees_json = attendees.to_json
+events_json = events.to_json
+# users = # Grab all users who have authorized us - need endpoints to grab all authorized users from eventbrite to do this
+
+# users.each do |user|
+#   @events = Eventbrite::User.owned_events({ user_id: user.id  })
+#   @attendees = Eventbrite::User.owned_event_attendees({ user_id: user.id  })
+# end
+
+converter = JsonConverter.new
+converter.write_to_csv attendees_json, 'attendees.csv'
+converter.write_to_csv events_json, 'events.csv'
